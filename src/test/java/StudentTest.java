3cf01501d84c0377f7a4c6ed3dc88233778c45f5
danielle lane <daniellelane1821@gmail.com>
2019-10-08 11:27:07 -0500

    finished with walk-through

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,40 +1,49 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
+    Student studentWithOneGrade;
+    Student studentWithtwoGrades;
+
+    @Before
+    public void setup () {
+        studentWithOneGrade = new Student(1L,"dani");
+        studentWithtwoGrades = new Student(2L,"fer");
+    }
 
     @Test
     public void testCreateStudent () {
-        Student dani = new Student(1L,"dani");
         Student ryan = null;
         assertNull(ryan);
-        assertNotNull(dani);
+        assertNotNull(studentWithOneGrade);
     }
 
     @Test
     public void testStudentFields(){
-        Student fer = new Student(1L, "fer");
-        assertSame(1L, fer.getId());
-        assertSame("fer", fer.getName());
-        assertSame(0, fer.getGrades().size());
+        assertSame(1L, studentWithOneGrade.getId());
+        assertSame("fer", studentWithOneGrade.getName());
+        assertSame(0, studentWithOneGrade.getGrades().size());
 
     }
 
     @Test
     public void testAddGrade () {
-        Student fer = new Student(1L, "fer");
-        fer.addGrade(100);
-        assertSame(100, fer.getGrades().get(0));
-        fer.addGrade(80);
-        assertSame(80, fer.getGrades().get(1));
+        studentWithOneGrade.addGrade(100);
+        assertSame(100, studentWithOneGrade.getGrades().get(0));
+        studentWithOneGrade.addGrade(80);
+        assertSame(80, studentWithOneGrade.getGrades().get(1));
     }
     @Test
     public void testAverageGrade(){
-        Student fer = new Student(1L, "fer");
-        fer.addGrade(100);
-        fer.addGrade(80);
-        assertEquals(90, fer.getGradeAverage(), 0);
+
+        studentWithOneGrade.addGrade(100);
+        studentWithtwoGrades.addGrade(80);
+        studentWithtwoGrades.addGrade(90);
+
+        assertEquals(100, studentWithOneGrade.getGradeAverage(), 0);
+        assertEquals(85, studentWithtwoGrades.getGradeAverage(), 0);
 
     }
 }
