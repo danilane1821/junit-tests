53d5905efe15f006e607738c3f8e079c548a8e38
danielle lane <daniellelane1821@gmail.com>
2019-10-08 09:42:52 -0500

    finished first part

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,8 +1,43 @@
+import java.util.ArrayList;
+import java.util.BitSet;
+
 public class Student {
 
 
-    public static long getId (long num) {
-        return num;
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
+
+    public long getId () {
+        return this.id;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public ArrayList<Integer> getGrades () {
+        return this.grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size() ;
+    }
+
+
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -5,11 +5,36 @@ public class StudentTest {
 
 
     @Test
-    public void testIfIdExist () {
+    public void testCreateStudent () {
+        Student dani = new Student(1L,"dani");
+        Student ryan = null;
+        assertNull(ryan);
+        assertNotNull(dani);
+    }
 
+    @Test
+    public void testStudentFields(){
+        Student fer = new Student(1L, "fer");
+        assertSame(1L, fer.getId());
+        assertSame("fer", fer.getName());
+        assertSame(0, fer.getGrades().size());
 
+    }
 
-
+    @Test
+    public void testAddGrade () {
+        Student fer = new Student(1L, "fer");
+        fer.addGrade(100);
+        assertSame(100, fer.getGrades().get(0));
+        fer.addGrade(80);
+        assertSame(80, fer.getGrades().get(1));
+    }
+    @Test
+    public void testAverageGrade(){
+        Student fer = new Student(1L, "fer");
+        fer.addGrade(100);
+        fer.addGrade(80);
+        assertEquals(90, fer.getGradeAverage(), 0);
 
     }
 }
